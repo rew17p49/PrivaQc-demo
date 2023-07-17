aecdf6869480102e795913dd63f24acd1c477248
rew17p49 <106513736+rew17p49@users.noreply.github.com>
2023-07-17 13:27:22 +0700

    port

--- assets/js/sockethost.js
+++ assets/js/sockethost.js
@@ -1,4 +1,5 @@
-const hostname = window.location.hostname + ":3000";
+// const hostname = window.location.hostname + ":3000";
+const hostname = window.location.hostname;
 const socketHost = window.location.protocol + "//" + hostname;
 
 console.log("Host: ",socketHost)
